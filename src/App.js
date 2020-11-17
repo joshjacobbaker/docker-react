aa1fb40e8e518b038bbd1ba8c4b24a81b23b84be
Josh <josh.jacob.baker@gmail.com>
2020-11-17 13:07:57 -0800

    corrected test result by adding 'learn react' within App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>I was changed on the feature branch</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn
+          learn react
         </a>
       </header>
     </div>
